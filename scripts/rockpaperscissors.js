d405def1b8d587131ddd17354e9b65439e2bf34b
Euan Pike <euanpike@gmail.com>
2021-06-20 19:16:48 +1200

    created a game function

--- scripts/rockpaperscissors.js
+++ scripts/rockpaperscissors.js
@@ -1,4 +1,7 @@
 let options = ['rock', 'paper', 'scissors']
+let playerScore = 0;
+let computerScore = 0;
+
 
 function computerPlay() {
     min = Math.ceil(0);
@@ -13,12 +16,43 @@ function singleRound(playerSelection, computerSelection) {
     }
 
     if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        playerScore++;
         return "You Win! Rock beats Scissors"
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        playerScore++;
         return "You Win! Paper beats Rock"
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        playerScore++;
         return "You Win! Scissors beats Paper"
     } else {
+        computerScore++;
         return "You Lose! " + computerSelection + " beats " + playerSelection
     }
 }
+
+function game() {
+    
+
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt('Rock, Paper, or Scissors...?');
+        while (!(options.includes(playerSelection.toLowerCase()))) {
+            alert("Please enter: Rock, Paper, or Scissors!");
+            playerSelection = prompt('Rock, Paper, or Scissors...?');
+        }
+        let computerSelection = computerPlay(); 
+        console.log(singleRound(playerSelection, computerSelection));
+    }
+
+    console.log("Your Score: " + playerScore);
+    console.log("Computer Score: " + computerScore);
+
+    if (playerScore > computerScore) {
+        console.log("You Won!");
+    } else if (computerScore > playerScore) {
+        console.log("You Lost! Try again?");
+    } else {
+        console.log("Its a Tie! Try again?");
+    }
+}
+
+game()
